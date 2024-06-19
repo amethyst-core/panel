0fc23f31ea0b4ee92c95783fa42dc6a0cee36276
Swargaraj Bhowmik <swargaraj66@gmail.com>
2024-06-19 17:48:59 +0530

    colors redefined

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <div>
-        <h1>Amethyst</h1>
+    <div className="flex h-screen w-screen items-center justify-center bg-[#1c1c22]">
+      <h2 className="text-5xl">
+        Amethyst <span className="text-primary-foreground">✦</span>
+      </h2>
     </div>
   );
 }
