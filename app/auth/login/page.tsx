22cba632ee1b8a67c00fdd83d3cbb0cb440e4bed
Swargaraj Bhowmik <swargaraj66@gmail.com>
2024-06-20 14:32:37 +0530

    test

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,5 +1,6 @@
-import * as React from "react";
 import Link from "next/link";
+
+import AuthHeader from "@/components/layout/auth/header";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -23,42 +24,68 @@ import Image from "next/image";
 
 export default function Login() {
   return (
-    <div className="h-[100vh]">
-      <header className="mx-auto flex items-center justify-between p-6 lg:px-8 border-b border-muted-foreground/10">
-        <Link href="/" className="text-lg">
-          Amethyst <span className="text-primary-foreground">✦</span>
-        </Link>
-        <div className="text-sm">
-          Don&apos;t have an account?
-          <Link
-            href="/auth/signup"
-            className="text-primary-foreground underline-offset-4 hover:underline ml-1"
-          >
-            Create New
-          </Link>
-        </div>
-      </header>
-      <div className="lg:flex lg:items-center lg:justify-between mt-6">
-        <div className="lg:flex-1 flex flex-col justify-space-between h-[100%] max-w-lg mx-12 my-10">
+    <>
+      <AuthHeader />
+      <div className="flex items-center pt-12 px-16">
+        <div className="flex flex-col justify-between flex-1 min-h-[70vh]">
           <div>
             <h1 className="text-5xl tracking-tight">
               Manage Your Amethyst Nodes With Ease
             </h1>
-            <p className="mt-4 text-lg text-muted-foreground">
+            <p className="mt-4 text-lg font-light text-muted-foreground">
               A modern, powerful, and user-friendly admin panel built for
               Minecraft servers.
             </p>
           </div>
 
           <div>
-            <p className="text-sm text-muted-foreground">
+            <p className="text-sm font-light text-muted-foreground">
               &copy; 2024 Amethyst. All rights reserved.
+              <span className="inline-block mt-3">
+                This is a demonstration purpose panel and may cause latency
+                issues and should not be used in production.
+              </span>
             </p>
           </div>
         </div>
-        <div className="mt-6 lg:mt-0 lg:ml-10"></div>
+
+        <div className="flex-2">
+          <Card>
+            <CardHeader>
+              <CardTitle>Sign in</CardTitle>
+              <CardDescription>
+                Enter your email and password to sign in
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <div className="grid gap-6">
+                <div className="grid grid-cols-1 gap-4">
+                  <div>
+                    <Label htmlFor="email">Email</Label>
+                    <Input
+                      id="email"
+                      type="email"
+                      placeholder="Enter your email"
+                    />
+                  </div>
+                  <div>
+                    <Label htmlFor="password">Password</Label>
+                    <Input
+                      id="password"
+                      type="password"
+                      placeholder="Enter your password"
+                    />
+                  </div>
+                </div>
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button type="submit">Sign in</Button>
+            </CardFooter>
+          </Card>
+        </div>
       </div>
       <div></div>
-    </div>
+    </>
   );
 }
